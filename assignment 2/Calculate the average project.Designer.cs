0b1cd1da28a43a80649c7ac273cd853c5f2b69be
abbey <a.middlestead@sjasd.ca>
2016-10-20 10:50:55 -0500

    calulating the average of marks
    calulating the average marks for grades

---
+++ assignment 2/Calculate the average project.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace assignment_2
+{
+    partial class frmCalculateTheAverageAssignment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt1stGrade = new System.Windows.Forms.TextBox();
+            this.txt2dGrade = new System.Windows.Forms.TextBox();
+            this.txt3rdGrade = new System.Windows.Forms.TextBox();
+            this.txt4thGrade = new System.Windows.Forms.TextBox();
+            this.txt5thGrade = new System.Windows.Forms.TextBox();
+            this.lbl1stGrade = new System.Windows.Forms.Label();
+            this.lbl2ndGrade = new System.Windows.Forms.Label();
+            this.lbl3rdGrade = new System.Windows.Forms.Label();
+            this.lbl4thGrade = new System.Windows.Forms.Label();
+            this.lbl5thGrade = new System.Windows.Forms.Label();
+            this.btnClaculateTheAverageForMarks = new System.Windows.Forms.Button();
+            this.lblCalculatedAverage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt1stGrade
+            // 
+            this.txt1stGrade.Location = new System.Drawing.Point(149, 12);
+            this.txt1stGrade.Name = "txt1stGrade";
+            this.txt1stGrade.Size = new System.Drawing.Size(100, 20);
+            this.txt1stGrade.TabIndex = 0;
+            // 
+            // txt2dGrade
+            // 
+            this.txt2dGrade.Location = new System.Drawing.Point(149, 38);
+            this.txt2dGrade.Name = "txt2dGrade";
+            this.txt2dGrade.Size = new System.Drawing.Size(100, 20);
+            this.txt2dGrade.TabIndex = 1;
+            // 
+            // txt3rdGrade
+            // 
+            this.txt3rdGrade.Location = new System.Drawing.Point(149, 64);
+            this.txt3rdGrade.Name = "txt3rdGrade";
+            this.txt3rdGrade.Size = new System.Drawing.Size(100, 20);
+            this.txt3rdGrade.TabIndex = 2;
+            // 
+            // txt4thGrade
+            // 
+            this.txt4thGrade.Location = new System.Drawing.Point(149, 90);
+            this.txt4thGrade.Name = "txt4thGrade";
+            this.txt4thGrade.Size = new System.Drawing.Size(100, 20);
+            this.txt4thGrade.TabIndex = 3;
+            // 
+            // txt5thGrade
+            // 
+            this.txt5thGrade.Location = new System.Drawing.Point(149, 117);
+            this.txt5thGrade.Name = "txt5thGrade";
+            this.txt5thGrade.Size = new System.Drawing.Size(100, 20);
+            this.txt5thGrade.TabIndex = 4;
+            // 
+            // lbl1stGrade
+            // 
+            this.lbl1stGrade.AutoSize = true;
+            this.lbl1stGrade.Location = new System.Drawing.Point(78, 19);
+            this.lbl1stGrade.Name = "lbl1stGrade";
+            this.lbl1stGrade.Size = new System.Drawing.Size(56, 13);
+            this.lbl1stGrade.TabIndex = 5;
+            this.lbl1stGrade.Text = "1st Grade:";
+            // 
+            // lbl2ndGrade
+            // 
+            this.lbl2ndGrade.AutoSize = true;
+            this.lbl2ndGrade.Location = new System.Drawing.Point(74, 45);
+            this.lbl2ndGrade.Name = "lbl2ndGrade";
+            this.lbl2ndGrade.Size = new System.Drawing.Size(60, 13);
+            this.lbl2ndGrade.TabIndex = 6;
+            this.lbl2ndGrade.Text = "2nd Grade:";
+            // 
+            // lbl3rdGrade
+            // 
+            this.lbl3rdGrade.AutoSize = true;
+            this.lbl3rdGrade.Location = new System.Drawing.Point(77, 71);
+            this.lbl3rdGrade.Name = "lbl3rdGrade";
+            this.lbl3rdGrade.Size = new System.Drawing.Size(57, 13);
+            this.lbl3rdGrade.TabIndex = 7;
+            this.lbl3rdGrade.Text = "3rd Grade:";
+            // 
+            // lbl4thGrade
+            // 
+            this.lbl4thGrade.AutoSize = true;
+            this.lbl4thGrade.Location = new System.Drawing.Point(77, 97);
+            this.lbl4thGrade.Name = "lbl4thGrade";
+            this.lbl4thGrade.Size = new System.Drawing.Size(57, 13);
+            this.lbl4thGrade.TabIndex = 8;
+            this.lbl4thGrade.Text = "4th Grade:";
+            // 
+            // lbl5thGrade
+            // 
+            this.lbl5thGrade.AutoSize = true;
+            this.lbl5thGrade.Location = new System.Drawing.Point(78, 120);
+            this.lbl5thGrade.Name = "lbl5thGrade";
+            this.lbl5thGrade.Size = new System.Drawing.Size(57, 13);
+            this.lbl5thGrade.TabIndex = 9;
+            this.lbl5thGrade.Text = "5th Grade:";
+            // 
+            // btnClaculateTheAverageForMarks
+            // 
+            this.btnClaculateTheAverageForMarks.Location = new System.Drawing.Point(61, 161);
+            this.btnClaculateTheAverageForMarks.Name = "btnClaculateTheAverageForMarks";
+            this.btnClaculateTheAverageForMarks.Size = new System.Drawing.Size(188, 23);
+            this.btnClaculateTheAverageForMarks.TabIndex = 10;
+            this.btnClaculateTheAverageForMarks.Text = "Calculate the average for marks";
+            this.btnClaculateTheAverageForMarks.UseVisualStyleBackColor = true;
+            this.btnClaculateTheAverageForMarks.Click += new System.EventHandler(this.btnClaculateTheAverageForMarks_Click);
+            // 
+            // lblCalculatedAverage
+            // 
+            this.lblCalculatedAverage.Location = new System.Drawing.Point(12, 199);
+            this.lblCalculatedAverage.Name = "lblCalculatedAverage";
+            this.lblCalculatedAverage.Size = new System.Drawing.Size(283, 147);
+            this.lblCalculatedAverage.TabIndex = 11;
+            // 
+            // frmCalculateTheAverageAssignment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 355);
+            this.Controls.Add(this.lblCalculatedAverage);
+            this.Controls.Add(this.btnClaculateTheAverageForMarks);
+            this.Controls.Add(this.lbl5thGrade);
+            this.Controls.Add(this.lbl4thGrade);
+            this.Controls.Add(this.lbl3rdGrade);
+            this.Controls.Add(this.lbl2ndGrade);
+            this.Controls.Add(this.lbl1stGrade);
+            this.Controls.Add(this.txt5thGrade);
+            this.Controls.Add(this.txt4thGrade);
+            this.Controls.Add(this.txt3rdGrade);
+            this.Controls.Add(this.txt2dGrade);
+            this.Controls.Add(this.txt1stGrade);
+            this.Name = "frmCalculateTheAverageAssignment";
+            this.Text = "Calculate the average assignment";
+            this.Load += new System.EventHandler(this.frmCalculateTheAverageMarks_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1stGrade;
+        private System.Windows.Forms.TextBox txt2dGrade;
+        private System.Windows.Forms.TextBox txt3rdGrade;
+        private System.Windows.Forms.TextBox txt4thGrade;
+        private System.Windows.Forms.TextBox txt5thGrade;
+        private System.Windows.Forms.Label lbl1stGrade;
+        private System.Windows.Forms.Label lbl2ndGrade;
+        private System.Windows.Forms.Label lbl3rdGrade;
+        private System.Windows.Forms.Label lbl4thGrade;
+        private System.Windows.Forms.Label lbl5thGrade;
+        private System.Windows.Forms.Button btnClaculateTheAverageForMarks;
+        private System.Windows.Forms.Label lblCalculatedAverage;
+    }
+}
+
---
+++ assignment 2/Calculate the average project.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment_2
+{
+    public partial class frmCalculateTheAverageAssignment : Form
+    {
+        public frmCalculateTheAverageAssignment()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculateGradeAverage_Click(object sender, EventArgs e)
+        /**
+
+*
+
+* Name: abbey middlestead
+
+* Teacher: mr. hardman
+
+* Assignment #2, Program #
+
+* Date Last Modified: 10/18/2016 at 10:10am
+
+*
+*/
+        {
+
+        }
+
+        private void frmCalculateTheAverageMarks_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClaculateTheAverageForMarks_Click(object sender, EventArgs e)
+        {
+            lbldisplay.Text = String.Format("{0,10}{1,10,}{2,10}{3,10}{4,10}{5,10}");
+
+             Firstnum.Text
+             Secondnum.Text
+             Thirdnum.Text
+             Fourthnum.Text
+             Fifthnum.Text
+
+
+
+
+
+
+        }
+    }
+}
